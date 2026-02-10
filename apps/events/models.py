56342c961c00587172ea717b93ab074e6039c1cb
djumanov <djumanovdev@gmail.com>
2026-02-10 10:49:04 +0500

    events model

--- apps/events/models.py
+++ apps/events/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    location = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='events/%Y/%m/%d/', blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='events')
+
+    def __str__(self):
+        return self.title
+    
+
+class Registration(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='registrations')
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='registrations')
+    registered_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'event')
+
+    def __str__(self):
+        return f'{self.user.full_name} - {self.event.title}'
+    
